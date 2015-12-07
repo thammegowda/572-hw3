e853a647924a6478d849985ebd8db7b4cee63807
nmante <nmante88@gmail.com>
2015-12-06 16:38:51 -0800

    Query not being added to url properly

--- charts/queryPasser.js
+++ charts/queryPasser.js
@@ -1,7 +1,8 @@
 function getQuery(){
 	var search = window.location.search;
 	$(".iFramePage").attr("src", $(".iFramePage").attr("src")+search);
-	window._globalQuery = search
+	window._globalQuery = search;
+	window.alert(search);
 }
 
 
