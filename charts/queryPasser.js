9b8c401d0ba3a2716db4a5f4872fe278e303df63
nmante <nmante88@gmail.com>
2015-12-06 17:15:23 -0800

    Using search text as query for iframes

--- charts/queryPasser.js
+++ charts/queryPasser.js
@@ -1,8 +1,8 @@
-function getQuery(){
+function getQuery(url){
 	var search = window.location.search;
 	$(".iFramePage").attr("src", $(".iFramePage").attr("src")+search);
 	window._globalQuery = search;
-	window.alert(search);
+	window.alert(url);
 }
 
 
